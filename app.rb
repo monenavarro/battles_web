246223e98ba64fe34ff6caf1f4f438032d50eb4d
monenavarro <me.navarro.97@outlook.com>
2022-04-08 12:15:17 +0100

    Extracting logic into project layer

--- app.rb
+++ app.rb
@@ -11,22 +11,22 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    redirect '/play'
+  end
+  
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
-
+  
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
-  end
-
-  post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    redirect '/play'
   end
   
   run! if app_file == $0
